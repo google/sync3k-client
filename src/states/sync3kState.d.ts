61419bec281bd40d23dccd8b621375aeb7220c0d
In-Ho Yi <chajath@gmail.com>
2017-07-21 00:10:30 -0700

    Initial import of store enhancer

---
+++ src/states/sync3kState.d.ts
@@ -0,0 +1,30 @@
+export type KeyDerivationSpec = {};
+
+export type Sync3kLocalState = {
+    // Key derivation fields.
+
+    /** Guid for the latest key derivation spec in use. */
+    latest: string,
+
+    /** Spec id to key derivation spec mapping. */
+    specs: {
+        [key: string]: KeyDerivationSpec,
+    },
+
+    // Key exchange fields.
+
+    /** Public key to pending key request mapping. */
+    keyRequests: {
+        [key: string]: {}
+    },
+
+    /** Public key of this client. */
+    key?: {}
+
+    // Sync ready state fields.
+
+    /** True if Sync3k engine is initialized. */
+    initialized?: boolean,
+};
+
+export type Sync3kState = { sync3k?: Sync3kLocalState };
